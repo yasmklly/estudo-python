0dd36bf089c7b42ad86a3d8b194cb40b845fba5c
Yasmim Mikelly <yasmim.mikelly08@gmail.com>
2025-08-22 16:50:17 -0300

    Add files via upload

---
+++ python_curso/nivel_dificil.py
@@ -0,0 +1,18 @@
+# 1. Montagem de Frase Personalizada
+# Questão: Peça o nome, a cidade e um hobby da pessoa. 
+# Mostre uma frase criativa unindo tudo.
+
+# nome = input('Digite seu nome: ')
+# cidade = input('Digite sua cidade: ')
+# hobby = input('Digite seu hobby preferido: ')
+
+# print(f'Olá! Meu nome é {nome}, eu moro em {cidade} e gosto muito de {hobby}!')
+
+# 2. Verificação de Palavra-Chave
+
+# Questão: Peça uma palavra ao usuário 
+# e verifique se ela contém a letra "a". Mostre True ou False.
+
+palavra = (input('Digite uma palavra: '))
+tem_a = 'a' in palavra.lower()
+print('A palavra contém a letra "a?', tem_a)
---
+++ python_curso/nivel_medio.py
@@ -0,0 +1,75 @@
+# Questão 1 (nível médio) -----------------
+# Peça ao usuário dois números inteiros e mostre: 
+# soma, subtração, multiplicação e divisão entre eles.
+
+# a = int(input('Digite o primeiro número: '))
+# b = int(input('Digite o segundo número: '))
+
+# print(f'Soma: {a + b}')
+# print(f'Multiplicação: {a * b}')
+# print(f'Subtração: {a - b}')
+# print(f'Divisão: {a / b}')
+
+
+# Questão 2 (nível médio) -----------------
+# Peça três notas (float), calcule a 
+# média e mostre se o aluno foi aprovado (média ≥ 7).
+
+# n1 = float(input('Digite a sua primeira nota: '))
+# n2 = float(input('Digite a sua segunda nota: '))
+# n3 = float(input('Digite a sua terceira nota: '))
+
+# media = (n1 + n2 + n3) / 3
+# print (f'Sua média é:{media:.2f}')
+# if (media >= 7):
+#     print('Parabéns! Você passou.')
+# else: 
+#     print('Você reprovou.')
+
+
+# Questão 3 (nível médio) -----------------
+# Peça um valor em reais (float) e mostre o equivalente em dólares, 
+# considerando a cotação fixa de R$ 5,00.
+
+# reais = float(input('Digite o valor: '))
+# dolar = (reais / 5)
+
+# print('O valor é:{:.2f}'.format(dolar))
+
+
+# Questão 4 (nível médio) -----------------
+# Peça o raio de um círculo (float) e 
+# calcule a área (fórmula: π * raio², use 3.14).
+
+# raio = float(input('Digite o raio do círculo: '))
+# area = 3.14 * (raio ** 2)
+# print(f"A área do círculo é: {area:.2f}")
+
+# Questão 5 (nível médio) -----------------
+# Questão: Peça três números e calcule a expressão:
+# (a + b) * c / 2
+# Compare com a + (b * c) / 2.
+
+# n1 = int(input('Digite o primeiro número: '))
+# n2 = int(input('Digite o segundo número: '))
+# n3 = int(input('Digite o terceiro número: '))
+
+# total1 = (n1 + n2) * n3/2
+# total2 = n1 + (n2 * n3) / 2
+
+# print(f'O resultado do primeiro total é: {total1}')
+# print(f'O resultado do segundo total é: {total2}')
+
+
+# Questão 5 (nível médio) -----------------
+# Peça a idade de uma pessoa e mostre:
+# Se ela é maior de idade (18 ou mais) → True
+# Caso contrário → False
+
+# idade = int(input('Digite a sua idade: '))
+
+# if (idade >= 18):
+#     print('Você é maior de idade.')
+
+# else:
+#     print('Você não é de maior.')
